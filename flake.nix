b9687c5374ad56038440a1ab89fd85ca2c708317
RocketRace <git@olivialta.cc>
2025-02-02 17:30:20 +0200

    nixify

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "my site";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (sys:
+    let
+      pkgs = nixpkgs.legacyPackages.${sys};
+      crate = pkgs.rustPlatform.buildRustPackage {
+        pname = "512";
+        version = "0.0.0";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = {
+            "ti-1.2.0" = "243292b03ea547c575f9e9508e693b5b35389c27a2f95ce5951835cff8925857";
+          };
+        };
+      };
+    in {
+      packages.default = crate;
+      devShells.default = pkgs.mkShell {
+        packages = [ crate ];
+      };
+    }
+  );
+}
